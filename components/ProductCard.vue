17174e43314631483b3655fccff6e57ceba4e1e3
Shaun <shaun@thenetninja.co.uk>
2022-11-11 18:30:39 +0000

    lesson-15

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -5,8 +5,10 @@
       <p class="text-2xl text-secondary">{{product.title}}</p>
       <p class="text-xl text-gray-50">{{product.description}}</p>
       <p class="text-lg text-secondary my-3">{{product.price}} Silver Coins</p>
-      <button class="btn" @click="addToBasket()">
-        <span>Add to Basket</span>
+      
+      <button class="btn" @click="addToBasket()" :disabled="isPending">
+        <span v-show="!isPending">Add to Basket</span>
+        <span v-show="isPending">Adding...</span>
       </button>
     </div>
   </div>
@@ -18,8 +20,13 @@
   const { product } = defineProps(['product'])
 
   const cartStore = useCartStore()
+  const isPending = ref(false)
 
   const addToBasket = async () => {
+    isPending.value = true
     await cartStore.addToCart(product)
+    setTimeout(() => {
+      isPending.value = false
+    }, 1000)
   }
 </script>
