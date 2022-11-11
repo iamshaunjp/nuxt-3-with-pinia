48f76d53ca7d134a03b70f66203313b7e73ef562
Shaun <shaun@thenetninja.co.uk>
2022-11-11 10:54:15 +0000

    lesson-5

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
     <h2 class="text-white text-2xl text-center my-8">Products</h2>
+
+    <div class="grid grid-cols-2 gap-7">
+      <div v-for="product in products">
+        {{ product.title }}
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-
+  // fetch products
+  const { data: products } = await useFetch('http://localhost:4000/products')
 </script>
 
 <style scoped>
